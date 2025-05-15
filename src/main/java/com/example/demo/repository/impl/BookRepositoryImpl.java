bf0f86f1eb36945db0c3833d40ca8056940e6df0
rmch <chigrin.ra@gmail.com>
2025-05-15 21:20:02 +0200

    feat: Create book repository, service and model (#2)

---
+++ src/main/java/com/example/demo/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,52 @@
+package com.example.demo.repository.impl;
+
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save book to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> getAllBooksQuery = session.createQuery("from Book", Book.class);
+
+            return getAllBooksQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't fetch all books from DB", e);
+        }
+    }
+}
