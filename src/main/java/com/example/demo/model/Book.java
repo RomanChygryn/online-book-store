e5ea29b1e55f5a8701fcb1606db0d20b52b5feaf
Roman Chygryn <chigrin.ra@gmail.com>
2025-05-15 12:08:09 +0200

    changes

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -85,40 +85,4 @@ public class Book {
     public String getCoverImage() {
         return coverImage;
     }
-
-    @Override
-    public String toString() {
-        return "Book{"
-                + "id=" + id
-                + ", title='"
-                + title
-                + '\''
-                + ", author='" + author + '\''
-                + ", isbn='" + isbn + '\''
-                + ", price=" + price
-                + ", description='" + description + '\''
-                + ", coverImage='" + coverImage + '\''
-                + '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Book book = (Book) o;
-        return Objects.equals(id, book.id)
-                && Objects.equals(title, book.title)
-                && Objects.equals(author, book.author)
-                && Objects.equals(isbn, book.isbn)
-                && Objects.equals(price, book.price)
-                && Objects.equals(description, book.description)
-                && Objects.equals(coverImage, book.coverImage);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, author, isbn, price, description, coverImage);
-    }
 }
